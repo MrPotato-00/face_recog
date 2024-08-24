13010df82c95913ffba215648129cf5831e7fc45
STADS706 <stads706@gmail.com>
2024-08-25 00:27:41 +0530

    Delete sample3.py

--- sample3.py
+++
@@ -1,7 +0,0 @@
-import datetime
-
-now = datetime.datetime.now()
-local_now = now.astimezone()
-local_tz = local_now.tzinfo
-local_tzname = local_tz.tzname(local_now)
-print(type(now), local_tzname)
